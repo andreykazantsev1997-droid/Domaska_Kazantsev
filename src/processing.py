70b913af374f09e87fa17800c42df7d4d10be2bb
Andrey Kazantsev <andrey.kazantsev.1997@gmail.com>
2026-03-28 14:03:24 +0300

    fix function and README-file

--- src/processing.py
+++ src/processing.py
@@ -11,3 +11,12 @@ def sort_by_date(list_dict: list, sort_list: bool = True) -> list:
     """Функция, которая сортирует списки словарей по дате"""
     sorted_list = sorted(list_dict, key=lambda x: x.get("date", ""), reverse=sort_list)
     return sorted_list
+
+test = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
+
+result = filter_by_skate(test)
+
+print(sort_by_date(result))
