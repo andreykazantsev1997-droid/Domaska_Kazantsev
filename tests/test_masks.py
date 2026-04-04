804df2426768719dbd87cd120ae963bfa830bbb0
Andrey Kazantsev <andrey.kazantsev.1997@gmail.com>
2026-04-04 10:20:28 +0300

    add test_mark.py

---
+++ tests/test_masks.py
@@ -0,0 +1,22 @@
+from unittest import expectedFailure
+import pytest
+
+from src.masks import get_mask_card_number, get_mask_account
+
+@pytest.mark.parametrize("number_card, expected", [("7000792289606361", "7000 79** **** 6361"),
+                         ("1234 5678 1234 5678", "1234 56** **** 5678"),
+                         ("1234567892123", "1234 56** **** 2123")])
+def test_get_mask_card_number_varied(number_card, expected):
+    assert get_mask_card_number(number_card) == expected
+
+def test_get_mask_card_number_empty():
+    with pytest.raises(ValueError) as exc_info:
+        get_mask_card_number("")
+        assert str(exc_info.value) == "Номер карты не введен"
+
+
+@pytest.mark.parametrize("number_account, expected", [("73654108430135874305", "**4305"),
+                         ("7365 4108 4301 3587 4305", "**4305"),
+                         ("7365410843013587", "**3587"),("3587", "**3587")])
+def test_get_mask_account_varied(number_account, expected):
+    assert get_mask_account(number_account) == expected
