a45e63cf26ad6bbd26421b55626bb22a3178d38f
Andrey Kazantsev <andrey.kazantsev.1997@gmail.com>
2026-04-04 10:21:13 +0300

    add pytest

--- src/masks.py
+++ src/masks.py
@@ -2,10 +2,13 @@ from typing import Union
 
 def get_mask_card_number(number_card: str) -> str:
     """Функция, которая маскирует цифры, кроме первых 6 цифр и последние 4 цифры банковской карты"""
-    unmasked_numbers = number_card[:6]
-    unmasked_numbers_2 = number_card[-4:]
+    card = number_card.replace(" ","")
+    if number_card == "":
+        raise ValueError("Номер карты не введен")
+    unmasked_numbers = card[:6]
+    unmasked_numbers_2 = card[-4:]
     masked_numbers = f"{unmasked_numbers}******{unmasked_numbers_2}"
-    return f"{masked_numbers[:4]} {masked_numbers[4:8]} {masked_numbers[8:12]} {masked_numbers[12:]}"
+    return f"{masked_numbers[:4]} {masked_numbers[4:6]}** **** {masked_numbers[-4:]}"
 
 
 def get_mask_account(number_account: str) -> str:
@@ -18,6 +21,5 @@ def get_mask_account(number_account: str) -> str:
 
 # card_number = str(input())
 # account_number = str(input())
-#
 # print(get_mask_card_number(card_number))
 # print(get_mask_account(account_number))
